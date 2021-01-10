04c541544a49e2682c91978d7458924cf27185e7
EasyBAMM <huhjb1020@naver.com>
2021-01-10 13:38:46 +0900

    mongo

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -5,9 +5,12 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+
+// const post = new Router(); // /api/posts/:id
+// posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
